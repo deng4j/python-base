6e5df0ff2cd748237b7c42500b843f77e98f5138
deng4j <1353976012@qq.com>
2022-12-19 00:59:10 +0800

    2022-12-19

--- Python_base/src/python/tools/FileDownload/AppMain.py
+++ Python_base/src/python/tools/FileDownload/AppMain.py
@@ -17,18 +17,25 @@ class MainDialog(QDialog):
         self.ui.textEdit_url.clear()
 
     def download(self):
+        self.ui.textEdit_notice.clear()
         path = self.ui.textEdit_dir.toPlainText()
         url = self.ui.textEdit_url.toPlainText()
         flag = self.urllib_download(IMAGE_URL=url, folderPath=path, suffix='.png')
-        self.ui.textEdit_notice.setTextColor(QtGui.QColor('red') if flag is False else QtGui.QColor('green'))
-        self.ui.textEdit_notice.setText('下载失败！！！' if flag is False else '下载成功')
+        if flag is False:
+            self.ui.textEdit_notice.setTextColor(QtGui.QColor('red'))
+            self.ui.textEdit_notice.setText('下载失败！！！')
+        else:
+            self.ui.textEdit_notice.setTextColor(QtGui.QColor('green'))
+            self.ui.textEdit_notice.setText('下载成功')
+            self.cleanURL()
 
     def urllib_download(self, IMAGE_URL, folderPath, suffix):
         from urllib.request import urlretrieve
         import ssl
         ssl._create_default_https_context = ssl._create_unverified_context
         import time
-        folderPath = folderPath + '/' + str(time.time()) + suffix
+        now = int(round(time.time() * 1000000))
+        folderPath = folderPath + '/' + str(now) + suffix
         try:
             urlretrieve(IMAGE_URL, folderPath)
         except:
