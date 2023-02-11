53d766f2e4b5e03744dc989cb9b9c5db88081331
deng4j <1353976012@qq.com>
2023-02-11 23:53:26 +0800

    2023-2-11

--- Python_base/src/python/BasicGrammar/other_modules/copy_mod/demo.py
+++ Python_base/src/python/BasicGrammar/other_modules/copy_mod/demo.py
@@ -7,15 +7,18 @@ import copy
 
 lst = [[0], [1]]
 
+print("lst对象id:", id(lst))
+print("lst索引0元素id:", id(lst[0]))
+
 print("---------------------- 浅拷贝 -----------------------")
 lst1 = lst.copy()  # 浅拷贝
 
-print(id(lst[0]))
-print(id(lst1[0]))
+print("lst1对象id:", id(lst1))
+print("lst1索引0元素id:", id(lst1[0]))
 
 print("---------------------- 深拷贝 -----------------------")
 
 lst2 = copy.deepcopy(lst)  # 深拷贝
 
-print(id(lst[0]))
-print(id(lst2[0]))
+print("lst2对象id:", id(lst2))
+print("lst2索引0元素id:", id(lst2[0]))
